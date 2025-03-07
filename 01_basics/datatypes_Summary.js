1bdcbc8b190b8ac535344dd59a3ba9a0ff815c4f
Garvit Ghai <221b160@juetguna.in>
2025-03-08 00:41:29 +0530

    Added datatypes_Summary to new location

---
+++ 01_basics/datatypes_Summary.js
@@ -0,0 +1,31 @@
+// Primitive
+
+// const id= Symbol('123')
+// const anotherId= Symbol('123')
+
+// console.log(id==anotherId);
+
+// const bigNUmber= 454643136312n
+// console.log(typeof bigNUmber)
+
+// const  outsideTemp= null
+// console.log(typeof(outsideTemp)) // object
+// 7 types: String, Number, BigInt, Boolearn , null , undefined, Symbol 
+
+//staticallly or dynamically typed?? ->js
+
+// Referances (non-primitive)
+// arrays, objects, functions.
+
+const cities= ["bengluru", "lko","jaipur"]
+console.log(typeof cities)
+
+const myfunction= function(){
+    console.log("hello world")
+}
+console.log(typeof myfunction)
+
+let myobj= {
+    name: "gasan",
+    age: 22,
+}
